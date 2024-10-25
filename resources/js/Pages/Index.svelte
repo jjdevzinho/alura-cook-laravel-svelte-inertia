db3664f470f40dd54ce94de37f3b6abccf9c56d9
jjdevzinho <jjdevzinho@gmail.com>
2024-10-25 20:19:44 -0300

    Iniciado com Suporte a js apenas

---
+++ resources/js/Pages/Index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { title } = $props();
+</script>
+
+<div class="flex flex-col items-center text-center space-y-4">
+    <h1 class="block mt-4 font-bold text-4xl">{title} Telo</h1>
+</div>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+        svelte(),
+    ],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./resources/js"),
+            "@assets": path.resolve(__dirname, "resources/assets"),
+        },
+    },
+});
